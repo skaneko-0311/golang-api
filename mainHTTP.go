cd97f06eff3b10c56f2e032afc5d1e4e33580da2
kanekos-Faber <kaneko@fabercompany.co.jp>
2019-02-26 23:40:18 +0900

    Add /health handler

--- mainHTTP.go
+++ mainHTTP.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 
+	// /healthハンドラ作成
+	http.HandleFunc("/health", healthHandler)
+
 	// 8080ポートで起動
 	http.ListenAndServe(":8080", nil)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World from Go.")
 }
