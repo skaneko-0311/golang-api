f0bc6a9aedd09122c948b974b8093f58e9e3bf08
skaneko-0311 <skaneko.0311@gmail.com>
2019-03-03 17:49:36 +0900

    modified:   mainHTTP_test.go

--- mainHTTP_test.go
+++ mainHTTP_test.go
@@ -11,13 +11,3 @@ func TestAddSuccess(t *testing.T) {
 		t.Fatal("failed test")
 	}
 }
-
-func TestAddFailed(t *testing.T) {
-	result, err := add(4, 8)
-	if err != nil {
-		t.Fatalf("failed test %#v", err)
-	}
-	if result != 12 {
-		t.Fatal("failed test")
-	}
-}
